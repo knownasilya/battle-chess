ad6b0ab93a11e1386c77626826831d67bf144cb9
Ilya Radchenko <knownasilya@gmail.com>
2021-12-30 11:14:08 -0500

    fix: index links

--- app/pods/room/route.ts
+++ app/pods/room/route.ts
@@ -1,5 +1,12 @@
 import Route from '@ember/routing/route';
 
+interface Params {
+  id: string;
+}
+
 export default class Room extends Route {
   // normal class body definition here
+  model({ id }: Params) {
+    return { id };
+  }
 }
