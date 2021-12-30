7e02ecaa0ea41529084c408be41b46bcd7164bf1
Ilya Radchenko <knownasilya@gmail.com>
2021-12-30 12:44:11 -0500

    feat: make it playable

--- app/components/board.ts
+++ app/components/board.ts
@@ -5,17 +5,23 @@ import { tracked } from '@glimmer/tracking';
 import * as Chess from 'chess.js';
 
 export default class Board extends Component {
-  @tracked selectedSquare?: string = undefined;
-  @tracked hoveredSquare?: string = undefined;
-  @tracked highlightedSquares?: string[] = undefined;
+  @tracked selectedSquare?: Chess.Square = undefined;
+  @tracked hoveredSquare?: Chess.Square = undefined;
+  @tracked highlightedSquares?: Chess.Square[] = undefined;
+  @tracked board: ({
+    type: Chess.PieceType;
+    color: 'b' | 'w';
+  } | null)[][];
 
   emptyList = [...Array(8)];
 
-  // @ts-expect-error known
-  chess: Chess.ChessInstance = new Chess();
+  chess: Chess.ChessInstance;
 
-  get board() {
-    return this.chess.board();
+  constructor(owner: object, args: object) {
+    super(owner, args);
+    // @ts-expect-error known
+    this.chess = new Chess();
+    this.board = this.chess.board();
   }
 
   getPiece = helper(([row, col]: [row: number, col: number]) => {
@@ -26,31 +32,35 @@ export default class Board extends Component {
   });
 
   @action
-  selectSquare(piece: Chess.Piece | null, square: string) {
+  selectSquare(piece: Chess.Piece | null, square: Chess.Square) {
+    // Make move if selecting a valid square
+    if (this.selectedSquare && this.highlightedSquares?.includes(square)) {
+      this.chess.move({ from: this.selectedSquare, to: square });
+      this.updateBoard();
+    }
+
+    this.highlightedSquares = getLegalMovesForPiece(this.chess, piece, square);
+
     if (this.selectedSquare === square) {
       this.selectedSquare = undefined;
       this.highlightedSquares = undefined;
     } else {
       this.selectedSquare = square;
-
-      if (piece) {
-        this.highlightedSquares = getLegalMovesForPiece(
-          this.chess,
-          piece,
-          square
-        );
-      } else {
-        this.highlightedSquares = undefined;
-      }
     }
+  }
+
+  updateBoard() {
+    this.board = this.chess.board();
   }
 }
 
 const getLegalMovesForPiece = (
   game: Chess.ChessInstance,
-  piece: Chess.Piece,
+  piece: Chess.Piece | null,
   square: string
 ) => {
+  if (!piece) return [];
+
   return game
     .moves({ verbose: true })
     .filter((move) => {
