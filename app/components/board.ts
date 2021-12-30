7225a3f02ad5c8c8e1b05a367f66013201c00aa6
Ilya Radchenko <knownasilya@gmail.com>
2021-12-30 09:21:53 -0500

    feat: draw pieces

--- app/components/board.ts
+++ app/components/board.ts
@@ -1,8 +1,24 @@
+import { helper } from '@ember/component/helper';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import * as Chess from 'chess.js';
 
 export default class Board extends Component {
   @tracked selectedSquare?: string = undefined;
 
   emptyList = [...Array(8)];
+
+  // @ts-expect-error known
+  chess: Chess.ChessInstance = new Chess();
+
+  get board() {
+    return this.chess.board();
+  }
+
+  getPiece = helper(([row, col]: [row: number, col: number]) => {
+    const board = this.board;
+    const piece = board[row][col];
+
+    return piece;
+  });
 }
---
+++ app/components/piece.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+interface PieceArgs {}
+
+export default class Piece extends Component<PieceArgs> {}
