7b026bce06d23e742f51a49deec001f086af1888
Ilya Radchenko <knownasilya@gmail.com>
2022-01-28 22:10:58 -0500

    fix: more frontile

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,5 +13,45 @@ module.exports = {
     require('@frontile/core/tailwind'),
     require('@frontile/overlays/tailwind'),
     require('@frontile/buttons/tailwind'),
+    require('@frontile/forms/tailwind'),
+    require('@frontile/notifications/tailwind'),
   ],
+  purge: {
+    options: {
+      safelist: [
+        // Frontile Core
+        /^close-button/,
+        /^visually-hidden/,
+
+        // Frontile Forms
+        /^form-field-checkbox/,
+        /^form-field-feedback/,
+        /^form-field-hint/,
+        /^form-field-input/,
+        /^form-field-label/,
+        /^form-field-radio/,
+        /^form-field-textarea/,
+        /^form-input/,
+        /^form-textarea/,
+        /^form-select/,
+        /^form-checkbox/,
+        /^form-radio/,
+        /^form-checkbox-group/,
+        /^form-radio-group/,
+
+        // Frontile Notifications
+        /^notifications-container/,
+        /^notification-card/,
+        /^notification-transition/,
+
+        // Frontile Overlays
+        /^overlay/,
+        /^modal/,
+        /^drawer/,
+
+        // Frontile Buttons
+        /^btn/,
+      ],
+    },
+  },
 };
