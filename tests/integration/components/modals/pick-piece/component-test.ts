3bdd64ee00257c0be5932daa9065ed5ea526786a
Ilya Radchenko <knownasilya@gmail.com>
2022-02-02 22:41:16 -0500

    fix: lint and foyer styles a bit

--- tests/integration/components/modals/pick-piece/component-test.ts
+++ tests/integration/components/modals/pick-piece/component-test.ts
@@ -3,14 +3,14 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | opponents/card-hand', function (hooks) {
+module('Integration | Component | modals/pick-piece', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Opponents::CardHand @cards={{array 0 1 2 3}}/>`);
+    await render(hbs`<Modals::PickPiece/>`);
 
     assert.dom(this.element).containsText('');
   });
